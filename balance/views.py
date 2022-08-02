ea4b54fbcaa015c4d586efeb0f475f33c930fa3b
Norberto <norbertoalves22@hotmail.com>
2022-08-02 21:55:20 +0200

    guardar avances

--- balance/views.py
+++ balance/views.py
@@ -1,12 +1,20 @@
-from . import app
+from flask import render_template  
 
+from . import app
+from .models import DBManager
+
+
+RUTA = 'data/balance.db'
 
 @app.route("/")
-def hola():
-    return "tabla de movimientos"
+def inicio():
+    db = DBManager(RUTA)
+    movimientos = db.consultaSQL("SELECT * FROM movientos")
+    return render_template("inicio.html", movs=movimientos)
+    # return "tabla de movimientos"
 
 
-@app.route("/purchase")
+@app.route("/purchase", methods=["GET", "POST"])
 def purchase():
     return "Tabla de convertir a monedas"
 
