319ebab38f217fe0e33f9a38e17b2c792be9aaf7
Norberto <norbertoalves22@hotmail.com>
2022-08-19 22:14:04 +0200

    creando la vista

--- criptocambio/views.py
+++ criptocambio/views.py
@@ -2,3 +2,14 @@ class CriptoView:
 
     def __init__(self):
         pass
+
+    def pedir_monedas(self):
+        origen = input("¿Que moneda quieres cambiar? ")
+        destino = input("¿Que moneda deseas obtener? ")
+        return (origen, destino)
+
+
+    def mostrar_cambio(self, origen, destino, cambio):
+        print("Un {} vale como {:,.2f} {}".format(
+            origen, cambio, destino,
+    ))
